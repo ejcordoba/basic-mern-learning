b675d44fceebcc09003804d5944112e7dcb60de8
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2024-06-06 18:14:42 +0200

    Add middlewares

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,4 +5,13 @@ const app = express();
 //Configuration
 app.set("port", process.env.PORT || 4000);
 
+//Middlewares
+app.use(cors());
+app.use(express.json());
+
+//Routes
+app.get("/", (req, res) => {
+  res.send("Welcome to my RESTful API");
+});
+
 module.exports = app;
