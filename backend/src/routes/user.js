9de6e4c4c1d1897bc59b18e27afebd8d3813bdf7
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2024-06-10 19:17:37 +0200

    call controllers on router

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,8 +1,16 @@
 const { Router } = require("express");
 const router = Router;
 
-router.route("/").get().post();
+const {
+  getUser,
+  getUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controller/user.controller");
 
-router.route("/:id").get().delete().put();
+router.route("/").get(getUsers).post(createUser);
+
+router.route("/:id").get(getUser).delete(deleteUser).put(updateUser);
 
 module.exports = router;
