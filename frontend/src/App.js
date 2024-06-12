cbc03dd4de9169aad4000df73ac7b5db81c91d72
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2024-06-12 17:14:38 +0200

    Define components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <h1>Hello world</h1>
+    </div>
+  );
 }
 
 export default App;
