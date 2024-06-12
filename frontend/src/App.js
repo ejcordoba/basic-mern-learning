75c85815bc0636c1eda32e6bdd9016ffa50a68ac
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2024-06-12 17:32:34 +0200

    Add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+import Navigation from "./components/Navigation";
+import ManageUsers from "./components/ManageUsers";
+import UsersList from "./components/UsersList";
+
+import { Container } from "@mui/material";
 
 function App() {
   return (
     <div className="">
-      <h1>Hello world</h1>
+      <Navigation />
+      <Container>
+        <Routes>
+          <Route path="/" element={<UsersList />}></Route>
+          <Route path="/CreateUser" element={<ManageUsers />}></Route>
+          <Route path="/EditUser/:id" element={<ManageUsers />}></Route>
+        </Routes>
+      </Container>
     </div>
   );
 }
